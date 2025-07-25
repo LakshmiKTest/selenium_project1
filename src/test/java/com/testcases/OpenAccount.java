d53a3e2013df0a7ee1e7d9c1d1eacb1c32de2908
Lakshmi <parthavisadhana.k2011@gmail.com>
2025-07-25 16:11:58 +0530

    Created testpatclass and added comments in the open account.java

--- src/test/java/com/testcases/OpenAccount.java
+++ src/test/java/com/testcases/OpenAccount.java
@@ -4,6 +4,8 @@ import org.testng.annotations.Test;
 
 import com.Basetest.BaseClass;
 
+# Open account class
+#Open class name
 public class OpenAccount extends BaseClass {
 	@Test
 	public void openAccount() {
---
+++ src/test/java/com/testcases/TestPatClass.java
@@ -0,0 +1,15 @@
+package com.testcases;
+
+import org.testng.annotations.Test;
+
+import com.Basetest.BaseClass;
+
+# Open TestPatClass class
+#Open TestPatClass name
+public class TestPatClass extends BaseClass {
+	@Test
+	public void TestPatClass() {
+		
+	}
+
+}
