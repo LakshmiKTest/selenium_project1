d0b51f052cfef3ec703a6ea604ed6efbfd8ca752
Lakshmi <parthavisadhana.k2011@gmail.com>
2025-07-25 16:14:40 +0530

    Update OpenAccount.java
    added master changes

--- src/test/java/com/testcases/OpenAccount.java
+++ src/test/java/com/testcases/OpenAccount.java
@@ -3,11 +3,12 @@ package com.testcases;
 import org.testng.annotations.Test;
 
 import com.Basetest.BaseClass;
-
+#  changes in the master branch
+ 
 public class OpenAccount extends BaseClass {
 	@Test
 	public void openAccount() {
-		
+		int masterBranch =0;
 	}
 
 }
